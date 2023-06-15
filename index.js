7b30488ef990ac2fc071dd2e2c1157b26fa87b6b
yves173 <59310013+yves173@users.noreply.github.com>
2023-06-15 12:11:41 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+//1.
+
+function joinStrings(string1, string2){
+   // code here
+  return`${string1} ${string2}`
+}
+
+
+//2.
+
+function sumOfDifferences(arr) {
+  if(arr.length<=1)
+    return 0
+  arr=arr.sort((a,b)=>b-a)
+  let sum=0
+  
+  for(let i=1;i<arr.length;i++)
+    sum+=(arr[i-1]-arr[i])
+  return sum
+}
+
+
+//3.
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.reduce((a,b)=>a+b) + arr2.reduce((a,b)=>a+b); //something went wrong
+}
+
+
+//4.
+
+lass SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+  }
+}
+
+
+//5.
+
+lass SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+  }
+}
